835eb160a5d362b18f23d03e0a0af07caf543951
whansolo <whansolo.tw@gmail.com>
2018-03-28 19:21:48 +0800

    left rotation
    https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem

---
+++ Cracking the Coding Interview/Arrays - Left Rotation/leftrotate.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    int k;
+    scanf("%d %d",&n,&k);
+    int *a = malloc(sizeof(int) * n);
+    for(int a_i = 0; a_i < n; a_i++){
+       scanf("%d",&a[a_i]);
+    }
+
+    int *b = malloc(sizeof(int) * n);
+    for(int i = 0; i < n; i++){
+        b[i] = a[(i + k) % n];
+        printf("%d ", a[(i + k) % n]);
+    }
+
+    return 0;
+}
