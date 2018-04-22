65313e729909c576d2cc13ba414d21e09829a621
whansolo <whansolo.tw@gmail.com>
2018-04-22 16:57:19 +0800

    updated: day27

--- 30 Days of Code/Day27 - Testing/printTestCase.cpp
+++ 30 Days of Code/Day27 - Testing/printTestCase.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     cout << "5" << endl;
-    cout << "5 3\n"; cout << "-2 1 3 9 0" <<endl;
-    cout << "4 3\n"; cout << "-1 9 20 1" <<endl;
-    cout << "3 3\n"; cout << "1 1 -3" <<endl;
-    cout << "6 2\n"; cout << "-2 0 1 3 9 0" <<endl;
-    cout << "4 1\n"; cout << "-2 0 0 9 0" <<endl;
-}
+    cout << "5 3" << endl; cout << "-1 90 999 100 0" << endl;
+    cout << "4 2" << endl; cout << "0 -1 2 1" << endl;
+    cout << "3 3" << endl; cout << "-1 0 1" << endl;
+    cout << "6 1" << endl; cout << "-1 0 1 -1 2 3" << endl;
+    cout << "7 3" << endl; cout << "-1 0 1 2 3 4 5" << endl;
+  }
